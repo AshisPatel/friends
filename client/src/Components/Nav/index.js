d62bad1e1a7f3bbdc64231be3f3608a7c66b46e0
AshisPatel <ashis.n.patel@gmail.com>
2021-12-10 10:08:25 -0600

    Style Nav component

---
+++ client/src/Components/Nav/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './style.css';
+
+const Nav = () => {
+    return (
+        <nav>
+            <h1>FRIENDS</h1>
+            <div className='nav-btn-container'>
+                <button>
+                    Add Friend
+                </button>
+            </div>
+        </nav>
+    );
+};
+
+export default Nav; 
